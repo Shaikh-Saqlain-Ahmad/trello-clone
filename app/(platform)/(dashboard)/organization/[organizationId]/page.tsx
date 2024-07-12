2eb6fe7d23eb0f597d4b390cbe17911a9a369664
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-07-12 21:15:57 +0500

    clerk organization added but missing middleware work

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,8 @@
+const OrganizatonIdPage=()=>{
+    return (
+        <div>
+            Organization Page
+        </div>
+    )
+}
+export default OrganizatonIdPage;
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,38 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware, clerkMiddleware, createRouteMatcher, redirectToSignIn } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
 const isProtectedRoute = createRouteMatcher([
     // add protected routes here
     
   ]);
-  export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
-  });
+  // export default clerkMiddleware((auth, req) => {
+  //   if (isProtectedRoute(req)) auth().protect();
+   
+  // });
+  export default authMiddleware({
+    publicRoutes:['/'],
+    afterAuth(auth,req){
+      if(auth.userId && auth.isPublicRoute){
+        let path=`/organization/${auth.orgId}`;
+        if(auth.orgId){
+          path=`/organinzation/${auth.orgId}`
+  
+        }
+        const orgSelection=new URL(path,req.url)
+        return NextResponse.redirect(orgSelection)
+      }
+      if(!auth.userId && !auth.isPublicRoute){
+        return redirectToSignIn({returnBackUrl:req.url})
+
+      }
+      if(auth.userId && auth.isPublicRoute && req.nextUrl.pathname!=="/select-org"){
+        const orgSelection=new URL("/select-org",req.url);
+        return NextResponse.redirect(orgSelection)
+      }
+      
+    }
+
+  })
 // export default clerkMiddleware();
 
 export const config = {
