9bd864805381d0e1ab04491e4e75d415915b4b64
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-08-06 14:09:40 +0500

    prisma setup completed with zod for schema

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,23 @@
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+
+
 const OrganizatonIdPage=()=>{
+   
     return (
         <div>
-            Organization Page
+            <form action={create}>
+                <input
+                id="title"
+                name="title"
+                required
+                placeholder="Enter a board title"
+                className="border-black border p-1"
+                />
+               <Button type="submit">
+                Submit
+               </Button>
+            </form>
         </div>
     )
 }
